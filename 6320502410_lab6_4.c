93ed8a3dc83330d4180d16bf03e7a114675cc274
Nitiphat2 <moozknot@gmail.com>
2021-03-18 16:22:23 +0700

    saddda

--- 6320502410_lab6_4.c
+++ 6320502410_lab6_4.c
@@ -15,29 +15,42 @@ int main()
         a[k-1]++;
     }
     int max=a[0],min=a[0];
+    j=0;
+    k=0;
     for(i=0; i<n; i++)
     {
-        if(max < a[i])
+        if(max <= a[i])
         {
-            max = a[i];
+            if(max == a[i])
+            {
+                if(j>i)
+                {
+                    j=i;
+                }
+            }
+            else if(max < a[i])
+            {
+               max = a[i];
+               j=i;
+            }
         }
-        if(min > a[i])
+        if(min >= a[i])
         {
-            min = a[i];
+            if(min == a[i])
+            {
+                if(k>i)
+                {
+                    k=i;
+                }
+            }
+            else if(min > a[i])
+            {
+               min = a[i];
+               k=i;
+            }
         }
     }
-    for(i=0; i<n; i++)
-    {
-        if(max == a[i])
-        {
-            printf("%d ",i+1);
-        }
-    }
-    for(i=0; i<n; i++)
-    {
-        if(min == a[i])
-        {
-            printf("%d",i+1);
-        }
-    }
+    printf("%d ",j+1);
+    printf("%d",k+1);
+    return 0;
 }
