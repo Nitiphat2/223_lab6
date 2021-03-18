48f5e89b7c6cf58c15257f4454e76c930c970200
Nitiphat2 <moozknot@gmail.com>
2021-03-18 16:04:28 +0700

    ตาลาย

--- 6320502410_lab6_4.c
+++ 6320502410_lab6_4.c
@@ -2,7 +2,12 @@
 int main()
 {
     int n,i,j,k;
-    scanf("%d",%n);
+    scanf("%d",&n);
     int a[n]
-    for()
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&j,&k);
+        a[j-1]++;
+        a[k-1]++;
+    }
 }
