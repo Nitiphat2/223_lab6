af315db3d2faf3aca756d8f2f259333083ab962a
Nitiphat2 <moozknot@gmail.com>
2021-03-18 15:38:02 +0700

    adsada

--- 6320502410_lab6_2.c
+++ 6320502410_lab6_2.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,k;
+    int n,i,j=0,k=0;
     scanf("%d",&n);
     char n1[n],n2[n];
     scanf(" %s",n1);
     scanf(" %s",n2);
-
+    for(i=0;i<n;i++)
+    {
+        if(n1[i]==n2[i])
+        {
+            j++;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    printf("%d %d",j,k);
 }
