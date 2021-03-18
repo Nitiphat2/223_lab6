8b24b99aadfb8365bb75b8f56a89c8972f4d3ede
Nitiphat2 <moozknot@gmail.com>
2021-03-18 16:10:05 +0700

    เสด4

--- 6320502410_lab6_4.c
+++ 6320502410_lab6_4.c
@@ -4,18 +4,18 @@ int main()
     int n,i,j,k;
     scanf("%d",&n);
     int a[n];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         a[i]=0;
     }
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         scanf("%d%d",&j,&k);
         a[j-1]++;
         a[k-1]++;
     }
     int max=a[0],min=a[0];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         if(max < a[i])
         {
@@ -24,6 +24,20 @@ int main()
         if(min > a[i])
         {
             min = a[i];
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        if(max == a[i])
+        {
+            printf("%d ",i+1);
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        if(min == a[i])
+        {
+            printf("%d",i+1);
         }
     }
 }
