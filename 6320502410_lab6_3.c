48dcc349ed354ab74229f4fbc6a22e9ef3304a1f
Nitiphat2 <moozknot@gmail.com>
2021-03-18 15:49:18 +0700

    sssss

--- 6320502410_lab6_3.c
+++ 6320502410_lab6_3.c
@@ -3,4 +3,11 @@ int main()
 {
     int n,q,i,j,k;
     scanf("%d%d",&n,&q);
+    char u;
+    int h[n];
+    for(i=0;i<q;i++)
+    {
+        scanf(" %c",&u);
+        scanf(" %d%d",&j,&k);
+    }
 }
