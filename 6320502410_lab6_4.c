86c8cbdce1b01ed5c625d7e065742fd76a5c3d0b
Nitiphat2 <moozknot@gmail.com>
2021-03-18 16:05:28 +0700

    aaa

--- 6320502410_lab6_4.c
+++ 6320502410_lab6_4.c
@@ -3,7 +3,11 @@ int main()
 {
     int n,i,j,k;
     scanf("%d",&n);
-    int a[n]
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        a[i]=0;
+    }
     for(i=0;i<n;i++)
     {
         scanf("%d%d",&j,&k);
