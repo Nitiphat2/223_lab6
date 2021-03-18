4438d64ca8fa5147e0694f92875877674037b3a7
Nitiphat2 <moozknot@gmail.com>
2021-03-18 15:12:59 +0700

    ss

--- 6320502410_lab6_1.c
+++ 6320502410_lab6_1.c
@@ -6,19 +6,27 @@ int main()
     scanf("%d",&n);
     char name[n][100],na[n][100];
     int i,j,k;
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         scanf("%s",name[i]);
     }
-    int h=0;
-    for(i=0;i<n;i++)
+    char h[100]="5a";
+    printf("\n");
+    for(i=0; i<n;i++)
     {
         for(j=i;j<n-1;j++)
         {
             if(strcmp(name[i],name[j+1])<0)
             {
-                h=j+1;
+                printf("%d %d\n",i,strcmp(name[i],name[j+1]));
+                strcpy(h,name[i]);
+                strcpy(name[i]," ");
+                strcpy(name[i],name[i+1]);
+                strcpy(name[i+1]," ");
+                strcpy(name[i+1],h);
             }
+            strcpy(h," ");
         }
+        printf("%s\n",name[i]);
     }
 }
