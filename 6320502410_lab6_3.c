8a3a274ab8cbceaf8dc484b4f51260594a6f0936
Nitiphat2 <moozknot@gmail.com>
2021-03-18 15:57:47 +0700

    ผิดเองครับ

--- 6320502410_lab6_3.c
+++ 6320502410_lab6_3.c
@@ -1,13 +1,39 @@
 #include<stdio.h>
 int main()
 {
-    int n,q,i,j,k;
+    int n,q,i,j,k,l,m,count=0;
     scanf("%d%d",&n,&q);
     char u;
-    int h[n];
+    int h[n],max,v[q];
+    for(i=0;i<n;i++)
+    {
+        h[i]=0;
+    }
     for(i=0;i<q;i++)
     {
         scanf(" %c",&u);
-        scanf(" %d%d",&j,&k);
+        scanf(" %d",&j);
+        scanf(" %d",&k);
+        if(u == 'U')
+        {
+            h[j-1]=k;
+        }
+        else if(u == 'P')
+        {
+            max =0;
+            for(l=j-1;l<k;l++)
+            {
+                if(max<h[l])
+                {
+                    max=h[l];
+                }
+            }
+            v[count]=max;
+            count++;
+        }
+    }
+    for(i=0;i<count;i++)
+    {
+        printf("%d\n",v[i]);
     }
 }
